1a89afbbd2634bcffbccd033269502ed2d484308
proteinj <jaehyun516@naver.com>
2026-06-07 23:40:28 +0900

    conflict 해결

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps 
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,11 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+    // 💡 1. GMSServices (s 붙임)
+    // 💡 2. 여기에 발급받으신 '진짜 구글맵 iOS API 키'를 직접 적어주세요.
+    GMSServices.provideAPIKey("AIzaSyCNYSv9LXcgTJSXJqBTqvmhzE1I5oxoVHM")
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
